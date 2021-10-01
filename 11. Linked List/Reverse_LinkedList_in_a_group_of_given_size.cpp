799166237d2e7f7e96ab4506057e67acf8ed64fc
Suruchi Sahu <55653153+suruchi2402@users.noreply.github.com>
2021-10-01 17:18:38 +0530

    Create Reverse_LinkedList_in_a_group_of_given_size.cpp

---
+++ 11. Linked List/Reverse_LinkedList_in_a_group_of_given_size.cpp
@@ -0,0 +1,92 @@
+// Question Link:-"https://practice.geeksforgeeks.org/problems/reverse-a-linked-list-in-groups-of-given-size/1"
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+struct node
+{
+    int data;
+    struct node* next;
+    
+    node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+/* Function to print linked list */
+void printList(struct node *node)
+{
+    while (node != NULL)
+    {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+}
+
+class Solution
+{
+    public:
+    struct node *reverse (struct node *head, int k)
+    { 
+        struct node *pre=NULL,*curr=head,*next;
+        
+            int c=k;
+            while(curr!=NULL && c){
+                next=curr->next;
+                curr->next=pre;
+                pre=curr;
+                curr=next;
+                c--;
+            }
+            if(head!=NULL){
+                head->next=reverse(next,k);
+            }
+            return pre;
+    }
+};
+
+/* Driver program to test above function*/
+int main(void)
+{
+    int t;
+    cin>>t;
+     
+    while(t--)
+    {
+        struct node* head = NULL;
+        struct node* temp = NULL;
+        int n;
+        cin >> n;
+         
+        for(int i=0 ; i<n ; i++)
+        {
+            int value;
+            cin >> value;
+            if(i == 0)
+            {
+                head = new node(value);
+                temp = head;
+            }
+            else
+            {
+                temp->next = new node(value);
+                temp = temp->next;
+            }
+        }
+        
+        int k;
+        cin>>k;
+        
+        Solution ob;
+        head = ob.reverse(head, k);
+        printList(head);
+    }
+     
+    return(0);
+}
+
+  // } Driver Code Ends
